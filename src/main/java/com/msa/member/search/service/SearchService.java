f38a82e5f4df57f1ec4cd7dc7bdd09520fe3a5c5
jinaru16 <wjdcksdnr11@naver.com>
2025-03-23 16:34:25 +0900

    submodule 제거

--- src/main/java/com/msa/member/search/service/SearchService.java
+++ src/main/java/com/msa/member/search/service/SearchService.java
@@ -2,10 +2,8 @@ package com.msa.member.search.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class SearchService {
     public void findAllMember() {
