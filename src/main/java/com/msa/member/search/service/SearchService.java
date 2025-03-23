72e0a1edfab5c7a10ad35c347ecbbcaa1d115e81
jinaru16 <wjdcksdnr11@naver.com>
2025-03-23 21:47:58 +0900

    aaa

--- src/main/java/com/msa/member/search/service/SearchService.java
+++ src/main/java/com/msa/member/search/service/SearchService.java
@@ -1,13 +1,20 @@
 package com.msa.member.search.service;
 
+import com.msa.member.search.repository.SearchRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.msa.common.entity.Users;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SearchService {
+    private final SearchRepository searchRepository;
+
+
     public void findAllMember() {
-      //  List<Users> all = searchRepository.findAll();
-        //all.forEach(System.out::println);
+        List<Users> all = searchRepository.findAll();
+        all.forEach(System.out::println);
     }
 }
